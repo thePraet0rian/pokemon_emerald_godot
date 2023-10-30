b3f7df43ac99be7e227724e326cb568cd2c179c5
thePraet0rian <thePraet0rian404@gmail.com>
2023-10-30 22:13:42 +0100

    More NPC's

--- components/npc.gd
+++ components/npc.gd
@@ -5,6 +5,7 @@ extends Area2D
 @export var direction: Vector2 = Vector2(0, 0)
 @export var offset: Vector2 = Vector2(0, 0)
 @export var npc: int = 0
+@export var frames: int = 0
 
 @onready var sprite: Sprite2D = $sprite_2d
 
@@ -13,30 +14,41 @@ var text: Array
 
 func _ready() -> void:
 	
+	sprite.hframes = frames
 	text.append_array(dialogue.npc_dialogue_string[npc])
 	
 	sprite.texture = txt
 	sprite.offset = offset
 	
-	if direction.x == 1:
-		sprite.frame = 3
-	elif direction.x == -1:
-		sprite.frame = 2
 	
-	if direction.y == 1:
-		sprite.frame = 0
-	elif direction.y == -1:
-		sprite.frame = 1
+	match frames:
+		
+		12:
+			if direction.x == 1:
+				sprite.frame = 3
+			elif direction.x == -1:
+				sprite.frame = 2
+			
+			if direction.y == 1:
+				sprite.frame = 0
+			elif direction.y == -1:
+				sprite.frame = 1
+		
+		9:
+			pass
 
 
 func update() -> void:
 	
-	if direction.x == -1:
-		sprite.frame = 3
-	elif direction.x == 1:
-		sprite.frame = 2
-	
-	if direction.y == -1:
-		sprite.frame = 0
-	elif direction.y == 1:
-		sprite.frame = 1
+	match frames:
+		
+		12:
+			if direction.x == -1:
+				sprite.frame = 3
+			elif direction.x == 1:
+				sprite.frame = 2
+			
+			if direction.y == -1:
+				sprite.frame = 0
+			elif direction.y == 1:
+				sprite.frame = 1
--- dialogue.gd
+++ dialogue.gd
@@ -1,14 +1,19 @@
 extends Node
 
 
-const npc_01: Array = [["This is a test\n", "This is a second test\n", "This is a third test\n", "This is a fourth test"], ["This is a asdflasdjf"]]
+const npc_01: Array = [["MOM: How are you doing, May?\n You look a little tired"], ["I think you should rest a bit"], ["Take care honey."]]
+const npc_02: Array = [["Are you going to catch Pokemon?\nGood Luck.\n"]]
+const npc_03: Array = [["If you use the PC, you can store items\n and Pokemon"], ["The Power of sience is staggering!"]]
 
-const npc_dialogue_string: Array =  [npc_01]
+const npc_dialogue_string: Array =  [npc_01, npc_02, npc_03]
 
 
 
+const object_01: Array = [["Dad might like this program.\n"], ["Better get going!"]]
+const object_02: Array = [["The time is already set.\n"]]
+const object_03: Array = [["It is a Nintendo GameCube\n"], ["A Game Boy Advance is connected\n to serve as the Controller.\n"]]
+const object_04: Array = [["The map is too dirty to read\n"]]
+const object_05: Array = [["The pc is unpluged.\n"]]
 
-const object_01: Array = [["This is a test\n"]]
-
-const object_dialogue_string: Array = [object_01]
+const object_dialogue_string: Array = [object_01, object_02, object_03, object_04, object_05]
 
